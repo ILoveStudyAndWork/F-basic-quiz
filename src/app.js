76c1ad9dd11ee73196c2fc3fb00742bfbb7660dc
ILoveStudyAndWork <fengyi.ling@thoughtworks.com>
2020-09-15 20:36:56 +0800

    [fengyi_ling] refactor:add createEducationItemList function

--- src/app.js
+++ src/app.js
@@ -16,32 +16,35 @@ async function getBasicInfo() {
   avatarURl.src = avatar;
 }
 
+function createEducationItem(year, title, description) {
+  const educationItem = document.createElement("section");
+  educationItem.setAttribute("class", "edu-item");
+
+  const yearDiv = document.createElement("div");
+  yearDiv.setAttribute("class", "year");
+  const eduDetail = document.createElement("article");
+  eduDetail.setAttribute("class", "edu-detail");
+  const eduTitle = document.createElement("p");
+  const eduDesc = document.createElement("p");
+  eduTitle.setAttribute("class", "edu-title");
+  eduDesc.setAttribute("class", "edu-desc");
+  yearDiv.textContent = year;
+  eduTitle.textContent = title;
+  eduDesc.textContent = description;
+  educationItem.appendChild(yearDiv);
+  eduDetail.appendChild(eduTitle);
+  eduDetail.appendChild(eduDesc);
+  educationItem.appendChild(eduDetail);
+  educationList.appendChild(educationItem);
+}
+
 async function getEducation() {
   const educationResponse = await fetch(educationUrl);
   const education = await educationResponse.json();
   // eslint-disable-next-line array-callback-return
   education.map((item) => {
     const { year, title, description } = item;
-    const educationItem = document.createElement("section");
-    educationItem.setAttribute("class", "edu-item");
-
-    const yearDiv = document.createElement("div");
-    yearDiv.setAttribute("class", "year");
-    const eduDetail = document.createElement("article");
-    eduDetail.setAttribute("class", "edu-detail");
-    const eduTitle = document.createElement("p");
-    const eduDesc = document.createElement("p");
-    eduTitle.setAttribute("class", "edu-title");
-    eduDesc.setAttribute("class", "edu-desc");
-    yearDiv.textContent = year;
-    eduTitle.textContent = title;
-    eduDesc.textContent = description;
-
-    educationItem.appendChild(yearDiv);
-    eduDetail.appendChild(eduTitle);
-    eduDetail.appendChild(eduDesc);
-    educationItem.appendChild(eduDetail);
-    educationList.appendChild(educationItem);
+    createEducationItem(year, title, description);
   });
 }
 
